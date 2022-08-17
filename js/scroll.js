c7b709372861be70e9a47d15712ef78a469b76d1
hyunwoo <archihw94@gmail.com>
2022-08-18 03:47:53 +0900

    8월 18일

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 const content = document.querySelector('.content');
 const header = document.querySelector('header');
 const tagWrapper = document.querySelector('.tagWrapper');
+const tagItem = document.querySelectorAll('.tagWrapper span');
 const content2Wrapper = document.querySelector('.content2-wrapper');
 const content2WrapperStart = content2Wrapper.getBoundingClientRect().top;
 
@@ -16,5 +17,7 @@ document.addEventListener('scroll', function(){
   scrollY > 500 ? header.classList.remove('active') : header.classList.add('active');
   const nav = document.querySelector('.nav');
   scrollY > 500 ? nav.classList.add('active') : nav.classList.remove('active');
-  scrollY > content2WrapperStart ? tagWrapper.classList.add('active') : tagWrapper.classList.remove('active');
+  for ( let i = 0; i < tagItem.length; i++) {
+    scrollY > content2WrapperStart ? tagItem[0].classList.add('active') : tagItem[0].classList.remove('active');
+  }
 })
