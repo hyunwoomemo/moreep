e0327137e7efe998c7937234f12a87446e0743ad
hyunwoo <archihw94@gmail.com>
2022-08-18 19:48:02 +0900

    nav 수정

--- js/scroll.js
+++ js/scroll.js
@@ -5,7 +5,7 @@ const tagItem = document.querySelectorAll('.tagWrapper span');
 const content2Wrapper = document.querySelector('.content2-wrapper');
 const content2WrapperStart = content2Wrapper.getBoundingClientRect().top;
 const nav = document.querySelector('.nav');
-const navTitle = nav.querySelector('a');
+const navSlideWrapper = document.querySelector('.nav-slide-wrapper');
 
 console.log(content2WrapperStart);
 
@@ -13,20 +13,18 @@ window.addEventListener('load', function(){
   content.classList.add('active');
 })
 
-let scrollValue = document.documentElement.scroll;
-
 document.addEventListener('scroll', function(){
-  scrollY > 500 ? header.classList.remove('active') : header.classList.add('active');
-  scrollY > 500 ? nav.classList.add('active') : nav.classList.remove('active');
-  scrollY > content2WrapperStart ? nav.classList.add('change') : nav.classList.remove('change');
-  if ( scrollY >= content2WrapperStart) {
-    nav.classList.add('change');
-    tagWrapper.classList.add('active');
-    navTitle.classList.add('hidden');
+  if ( scrollY > 500 ) {
+    header.classList.remove('active');
+    nav.classList.add('active');
+  } else {
+    header.classList.add('active');
+    nav.classList.remove('active');
   }
-  else {
-    nav.classList.remove('change');
-    tagWrapper.classList.remove('active');
-    navTitle.classList.remove('hidden');
+
+  if ( scrollY > content2WrapperStart) {
+    navSlideWrapper.classList.add('tag1');
+  } else if ( scrollY < content2WrapperStart) {
+    navSlideWrapper.classList.remove('tag1');
   }
 })
